10b2ffa268f7b03ad3090eda267270d089be18bf
christian.villegas_p <christian.villegas@pragma.com.co>
2024-10-16 17:12:02 -0500

    Se añaden los componentes characters y character

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 import Navbar from "./components/Navbar"
+import React, { useEffect, useState } from "react";
+import Characters from "./components/Characters";
+import FetchCharacters from "./services/characters-service";
 
 function App() {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    const getCharacters = async () => {
+      const data = await FetchCharacters();
+      setCharacters(data);
+    };
+    getCharacters();
+  }, []);
+  
   return (
-    <Navbar brand="Rick and Morty App"/>
+    <>
+      <Navbar brand="Rick and Morty App"/>
+      <div className="container">
+        <Characters characters={characters}/>
+      </div>
+    </>
+    
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,5 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
 
 const Navbar = ({ brand }) => {
-
-  const [characters, setCharacters] = useState([]);
-
-  const urlBase = "https://rickandmortyapi.com/api/character";
-
-  const fetchCharacters = async (url) => {
-    await axios.get(url)
-      .then((response) => {
-        const data = response.data.results;
-        setCharacters(data);
-        console.log(data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-  useEffect(() => {
-    fetchCharacters(urlBase);
-  }, [])
   return (
     <nav className="navbar navbar-dark bg-dark">
       <div className="container">
