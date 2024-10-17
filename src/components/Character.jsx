73cb60aab253ba8bed224948fe459d3f5e5267f4
christian.villegas_p <christian.villegas@pragma.com.co>
2024-10-16 21:19:24 -0500

    Se añaden los componentes characterData

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import CharacterData from "./CharacterData";
 
 const Character = ({ character, index }) => {
   return (
-    <div key={index} className='col'>
-      <div className="Card" style={{ minWidth: "180px" }}>
+    <div key={index} className="col mb-4">
+      <div className="card" style={{ minWidth: "200px" }}>
         <img src={character.image} alt="" />
+        <CharacterData 
+          name={character.name}
+          specie={character.species}
+          location={character.location.name}
+        />
       </div>
     </div>
   );
