829a742cf28e4e5252abe8617b76b4350457be73
leondominguez <leon.dominguez@correounivalle.edu.co>
2024-10-20 22:48:26 -0500

    fix library import

--- src/__tests__/pagination/Pagination.test.jsx
+++ src/__tests__/pagination/Pagination.test.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom'; // Importa jest-dom para usar toBeInTheDocument
 import Pagination from '../../components/Pagination';
 
 describe('Pagination Component', () => {
@@ -17,9 +18,9 @@ describe('Pagination Component', () => {
   });
 
   test('does not render Prev button when prev prop is not provided', () => {
-    render(<Pagination prev={null} next="some-url" onPrev={mockOnPrev} onNext={mockOnNext} />);
+    render(<Pagination prev={null} next={null} onPrev={mockOnPrev} onNext={mockOnNext} />);
     const prevButton = screen.queryByText(/Prev/i);
-    expect(prevButton).toBeNull();
+    expect(prevButton).not.toBeInTheDocument();
   });
 
   test('renders Next button when next prop is provided', () => {
@@ -29,9 +30,9 @@ describe('Pagination Component', () => {
   });
 
   test('does not render Next button when next prop is not provided', () => {
-    render(<Pagination prev="some-url" next={null} onPrev={mockOnPrev} onNext={mockOnNext} />);
+    render(<Pagination prev={null} next={null} onPrev={mockOnPrev} onNext={mockOnNext} />);
     const nextButton = screen.queryByText(/Next/i);
-    expect(nextButton).toBeNull();
+    expect(nextButton).not.toBeInTheDocument();
   });
 
   test('calls onPrev when Prev button is clicked', () => {
